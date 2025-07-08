fc01a3201a1650b31b47bd9bdb43e748dddea86d
yuvi2512 <joshiyatish25@gmail.com>
2025-07-08 18:14:14 +0530

    change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import { AuthProvider } from '@/context/AuthContext';
 import Head from 'next/head';
-import CssBaseline from '@mui/material/CssBaseline';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
@@ -8,9 +7,7 @@ export default function App({ Component, pageProps }) {
     <AuthProvider>
       <Head>
         <title>Admin Dashboard</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <CssBaseline />
       <Component {...pageProps} />
     </AuthProvider>
   );
